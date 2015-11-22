255ff8485b379b26a344f8502d508ed44482a24e
worapon <woraponok@outlook.com>
2015-11-22 22:10:13 +0700

    detect nav fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
+    $(window).scroll(function() {
+        var viewPortHeight = $(window).height();
+        var scrollHeight = $(window).scrollTop();
 
+        if (scrollHeight > viewPortHeight - 35) {
+            $('#navbar').addClass('navbar-fixed-top');
+        } else {
+            $('#navbar').removeClass('navbar-fixed-top');
+        }
+    });
 });
